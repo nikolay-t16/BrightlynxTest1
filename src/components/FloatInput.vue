c97edbc5ccd6793bd58cb05f20bd52ba8a912567
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-04-28 01:47:07 +0300

    Поле для ввода числа

---
+++ src/components/FloatInput.vue
@@ -0,0 +1,27 @@
+<template>
+	<input v-model=value type="text" v-on:keydown="onKeydown"/>
+</template>
+
+<script>
+	export default {
+		name: "FloatInput",
+		props: ['value'],
+		data: function () {
+			return {}
+		},
+		methods: {
+			onKeydown: function (e) {
+				if (!isNaN(e.key))
+					return;
+				if (e.key == "." && this.value.indexOf(".") ===  -1) {
+					return;
+				}
+				e.preventDefault();
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
