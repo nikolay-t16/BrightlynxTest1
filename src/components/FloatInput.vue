9f6dadb20dfffd133e9e43e6309f4ceca19d60e8
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-04-28 13:38:18 +0300

    Поле FloatInput

--- src/components/FloatInput.vue
+++ src/components/FloatInput.vue
@@ -1,25 +1,37 @@
 <template>
-	<input v-model=value type="text" v-on:keydown="onKeydown"/>
+    <input v-model=val type="text" placeholder="введите число (1.5)" v-on:keydown="onKeydown" v-on:keyup="onKeyup" />
 </template>
 
 <script>
-	export default {
-		name: "FloatInput",
-		props: ['value'],
-		data: function () {
-			return {}
-		},
-		methods: {
-			onKeydown: function (e) {
-				if (!isNaN(e.key))
-					return;
-				if (e.key == "." && this.value.indexOf(".") ===  -1) {
-					return;
-				}
-				e.preventDefault();
-			}
-		}
-	}
+  export default {
+    name: 'FloatInput',
+    props: {
+      value: Number
+    },
+    data: function () {
+      this.validKey = ["Delete", "Backspace", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+      return {
+        val: this.value
+      }
+    },
+    methods: {
+      onKeydown: function (e) {
+        if(this.validKey.indexOf(e.key) != -1 ||
+         isNumeric(e.key) ||
+          e.key == "." && this.val.indexOf(".") == -1
+        )
+          return;
+        e.preventDefault();
+      },
+      onKeyup: function () {
+        this.$emit('change', { val: this.val })
+      }
+    }
+  }
+
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
 </script>
 
 <style scoped>
